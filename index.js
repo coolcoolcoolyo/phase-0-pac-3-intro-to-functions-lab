ed6220451560171227ffce6877e0b437cdd34a57
coolcoolcoolyo <alexjspagnoli@gmail.com>
2022-09-09 03:23:27 -0400

    commit tree bb3b424be3ac7172c5d1bbcbb4f350352c6902f6

---
+++ index.js
@@ -0,0 +1,46 @@
+function shout(string) {
+    //hello
+    }
+
+function shout(string) {
+    return string.toUpperCase();
+    }
+
+function whisper(string) {
+    //HELLO
+    }
+
+function whisper(string) {
+    return string.toLowerCase();
+    }
+
+function whisper(string) {
+    return string.toLowerCase();
+    }
+
+const string = "HELLO"
+
+console.log(string);
+console.log(string.toUpperCase());
+
+console.log(string === string.toUpperCase());
+
+
+function logShout(string) {
+    console.log(string.toUpperCase( ))
+    }
+
+function logWhisper(string) {
+    console.log(string.toLowerCase( ))
+    }
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase( )) === string); 
+        console.log('I can\'t hear you!')
+    }
+    if  (string.toUpperCase( ) === 'HELLO'); {
+        console.log('YES INDEED!')
+    }
+    else {
+        console.log('I would love to!')
+    }
